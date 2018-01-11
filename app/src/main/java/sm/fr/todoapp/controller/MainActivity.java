c52452091a6ce3fce25e2fc9466891bce3b901d0
formation <formateur@mail.com>
2018-01-11 13:46:57 +0100

    Création du formulaire d'ajout de tâche

--- app/src/main/java/sm/fr/todoapp/controller/MainActivity.java
+++ app/src/main/java/sm/fr/todoapp/controller/MainActivity.java
@@ -1,15 +1,24 @@
 package sm.fr.todoapp.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import sm.fr.todoapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int TASK_FORM = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onNewTask(View v){
+        Intent intentNewTask = new Intent(this, TaskFormActivity.class);
+        startActivityForResult(intentNewTask, TASK_FORM);
     }
 }
---
+++ app/src/main/java/sm/fr/todoapp/controller/TaskFormActivity.java
@@ -0,0 +1,17 @@
+package sm.fr.todoapp.controller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import sm.fr.todoapp.R;
+
+public class TaskFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_form);
+    }
+}
