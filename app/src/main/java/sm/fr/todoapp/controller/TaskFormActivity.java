04cd8948e03677783a95b7d0fe6b7360dbec2a98
formation <formateur@mail.com>
2018-01-11 14:00:01 +0100

    Gestion de la persistance des tâche à la validation du formulaire

--- app/src/main/java/sm/fr/todoapp/controller/TaskFormActivity.java
+++ app/src/main/java/sm/fr/todoapp/controller/TaskFormActivity.java
@@ -1,17 +1,60 @@
 package sm.fr.todoapp.controller;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import sm.fr.todoapp.R;
+import sm.fr.todoapp.model.DatabaseHandler;
+import sm.fr.todoapp.model.Task;
+import sm.fr.todoapp.model.TaskDAO;
 
 public class TaskFormActivity extends AppCompatActivity {
+
+    EditText editTextTaskName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_form);
+
+        editTextTaskName = findViewById(R.id.editTextTask);
+    }
+
+    public void onValidForm(View view) {
+        String taskName = this.editTextTaskName.getText().toString();
+
+        if (taskName.trim() == "") {
+            String message = "La tâche ne peut être vide";
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        } else {
+            Task task = new Task(taskName);
+            this.processForm(task);
+        }
+    }
+
+    private void processForm(Task task){
+        DatabaseHandler db = new DatabaseHandler(this);
+        TaskDAO dao = new TaskDAO(db);
+        String message;
+
+        try {
+            dao.persist(task);
+            setResult(RESULT_OK);
+            message = "Tâche enregistrée";
+        } catch (SQLiteException ex) {
+            setResult(RESULT_CANCELED);
+            Log.d("DEBUG", ex.getMessage());
+            message = "Impossible d'enregistrer la tâche";
+
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
